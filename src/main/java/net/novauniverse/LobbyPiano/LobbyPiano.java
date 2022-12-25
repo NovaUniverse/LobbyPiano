3a991b3eacd4f8f25e58178fa8dc01b225b47c82
Dev Goldy <jassim7256@gmail.com>
2022-12-25 23:55:04 +0000

    changed pitches and added extra note

--- src/main/java/net/novauniverse/LobbyPiano/LobbyPiano.java
+++ src/main/java/net/novauniverse/LobbyPiano/LobbyPiano.java
@@ -28,48 +28,54 @@ public final class LobbyPiano extends JavaPlugin implements Listener {
 
     @EventHandler
     public void PlayerOnKeyEvent(PlayerMoveEvent event) {
-        if (event.getTo().getX() == event.getFrom().getX() || event.getTo().getZ() == event.getFrom().getZ()) {
+        if (event.getTo().getBlockX() == event.getFrom().getBlockX() || event.getTo().getBlockZ() == event.getFrom().getBlockZ()) {
             return;
         }
 
-        Location moveToEventLocation = event.getTo();
-        Location moveFromEventLocation = event.getFrom();
+        Location moveToEventLocation = event.getTo().clone().add(0, 0.5, 0);
 
-        World theWorldUwU = moveFromEventLocation.getWorld();
+        if (pianoPlayArea.isInsideBlock(moveToEventLocation.toVector())) {
+            Location moveFromEventLocation = event.getFrom().clone().add(0, 0.5, 0);
 
-        Block lastBlockBelowPlayer = moveFromEventLocation.getBlock().getRelative(BlockFace.DOWN);
-        Block currentBlockBelowPlayer = moveToEventLocation.getBlock().getRelative(BlockFace.DOWN);
+            World theWorldUwU = moveFromEventLocation.getWorld();
 
-        if (pianoPlayArea.isInside(currentBlockBelowPlayer.getLocation().toVector())) {
-            if (getDyeColour(currentBlockBelowPlayer) == getDyeColour(lastBlockBelowPlayer)) {
+            Block lastBlockBelowPlayer = moveFromEventLocation.getBlock().getRelative(BlockFace.DOWN);
+            Block currentBlockBelowPlayer = moveToEventLocation.getBlock().getRelative(BlockFace.DOWN);
+
+            DyeColor dyeColourForBlockBelow = getDyeColour(currentBlockBelowPlayer);
+
+            if (dyeColourForBlockBelow == getDyeColour(lastBlockBelowPlayer) || dyeColourForBlockBelow == null) {
                 return;
             }
 
             switch(getDyeColour(currentBlockBelowPlayer)) {
-                case BLUE:
-                    playNote(theWorldUwU, currentBlockBelowPlayer, 1.0F);
+                case LIGHT_BLUE:
+                    playNote(theWorldUwU, currentBlockBelowPlayer, 0.707107F);
                     break;
                 case CYAN:
-                    playNote(theWorldUwU, currentBlockBelowPlayer, 1.12246F);
+                    playNote(theWorldUwU, currentBlockBelowPlayer, 0.793701F);
                     break;
                 case PURPLE:
-                    playNote(theWorldUwU, currentBlockBelowPlayer, 1.25992F);
+                    playNote(theWorldUwU, currentBlockBelowPlayer, 0.890899F);
                     break;
                 case ORANGE:
-                    playNote(theWorldUwU, currentBlockBelowPlayer, 1.33484F);
+                    playNote(theWorldUwU, currentBlockBelowPlayer, 0.945874F);
                     break;
                 case RED:
-                    playNote(theWorldUwU, currentBlockBelowPlayer, 1.49831F);
+                    playNote(theWorldUwU, currentBlockBelowPlayer, 1.059463F);
                     break;
                 case PINK:
-                    playNote(theWorldUwU, currentBlockBelowPlayer, 1.68179F);
+                    playNote(theWorldUwU, currentBlockBelowPlayer, 1.189207F);
                     break;
                 case MAGENTA:
-                    playNote(theWorldUwU, currentBlockBelowPlayer, 1.88775F);
+                    playNote(theWorldUwU, currentBlockBelowPlayer, 1.334840F);
                     break;
+                case BLUE:
+                    playNote(theWorldUwU, currentBlockBelowPlayer, 1.414214F);
 
                 case BLACK:
                     playBlackNotes(theWorldUwU, currentBlockBelowPlayer);
+
             }
         }
     }
@@ -96,27 +102,29 @@ public final class LobbyPiano extends JavaPlugin implements Listener {
     private void playBlackNotes(World world, Block blockBelowPlayer) {
 
         switch (getDyeColour(blockBelowPlayer.getRelative(DWCNIRTBN))) {
-            case BLUE:
-                playBlackNote(world, blockBelowPlayer, 1.77239F);
+            case LIGHT_BLUE:
+                playNote(world, blockBelowPlayer, 0.707107F);
                 break;
             case CYAN:
-                playBlackNote(world, blockBelowPlayer, 1.8016F);
+                playNote(world, blockBelowPlayer, 0.793701F);
                 break;
             case PURPLE:
-                playBlackNote(world, blockBelowPlayer, 1.84524F);
+                playNote(world, blockBelowPlayer, 0.890899F);
                 break;
             case ORANGE:
-                playBlackNote(world, blockBelowPlayer, 1.89022F);
+                playNote(world, blockBelowPlayer, 0.945874F);
                 break;
             case RED:
-                playBlackNote(world, blockBelowPlayer, 1.94586F);
+                playNote(world, blockBelowPlayer, 1.059463F);
                 break;
             case PINK:
-                playBlackNote(world, blockBelowPlayer, 2.05146F);
+                playNote(world, blockBelowPlayer, 1.189207F);
                 break;
             case MAGENTA:
-                playBlackNote(world, blockBelowPlayer, 2.12244F);
+                playNote(world, blockBelowPlayer, 1.334840F);
                 break;
+            case BLUE:
+                playNote(world, blockBelowPlayer, 1.414214F);
         }
     }
 
